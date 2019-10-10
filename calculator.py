21817d04604547f09a9120e313b0b2a24e82b998
AJ Thompson <thoantj@dunwoody.edu>
2019-10-10 13:52:50 -0500

    adding main class

---
+++ calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def preProccess(self, input):
+        output = input  # TODO: Actually edit the input
+        return output
+
+
+    def process(self, input):
+        input = eval(input)
+        return input
