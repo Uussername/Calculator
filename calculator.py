16dad1195a786322c76b9dd5f8c7149a7de0e32f
AJ Thompson <thoantj@dunwoody.edu>
2019-10-14 14:36:45 -0500

    added regex statements to preProcess function

--- calculator.py
+++ calculator.py
@@ -1,10 +1,15 @@
 import os
+import re
 from builtins import eval, input, Exception, str
 
 
 class Calculator:
     def preProcess(self, input):
-        output = input  # TODO: Actually edit the input
+        n = 0
+        output = re.sub('(?<=\w|\))(?=\() | (?<=\))(?=\w) | (?<=\d)(?=n)', '*', input, flags=re.X)
+        pattern = re.compile('n', flags=re.IGNORECASE)
+        output = pattern.sub(str(n), output)
+
         return output
 
 
@@ -18,7 +23,7 @@ if __name__ == "__main__":
     while True:
         i = input("> ")
         if i.lower() == 'quit':
-            os.system("shutdown /s /t 1")
+            #os.system("shutdown /s /t 1")
             break
         try:
             o = calc.preProcess(i)
