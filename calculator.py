d50e9dafbec80ee39df3a95b80b2e2d3a7a27417
AJ Thompson <thoantj@dunwoody.edu>
2019-10-29 10:01:15 -0500

    preproccess removes commas and corrected some test cases to requirements

--- calculator.py
+++ calculator.py
@@ -7,6 +7,7 @@ from sys import platform
 class Calculator:
     def preProcess(self, input, n):  # TODO: Check for any disallowed characters specifically ','
         i = input.replace(" ", "")
+        i = i.replace(',','')
         if len(i) >= 128:
             raise Exception('input too large, must be under 128 characters')
         else:
